2e942df8ffc2eb7dff3beee7ebce012cb9385f2c
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2025-01-15 18:16:41 +0000

    First commit

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React from "react"; // 1. Import react
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//2. Create your first component with PascalCase
 const Home = () => {
+
+	//3. JS code
+
+	//4. Return a single JS component
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -23,4 +27,9 @@ const Home = () => {
 	);
 };
 
+//5. Export the component to use it
 export default Home;
+
+//In the file to use the component 
+//6.
+//7.
